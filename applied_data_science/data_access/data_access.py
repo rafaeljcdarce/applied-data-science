c1f7a5eca2af12c1ee694ebbaf4bb012ddd93c7b
Riley Evans <riley@rileyevans.co.uk>
2021-04-12 11:15:43 +0100

    Adds a new method to access vaccine data

--- applied_data_science/data_access/data_access.py
+++ applied_data_science/data_access/data_access.py
@@ -9,15 +9,8 @@ PROJECT_ID = "ads-coursework-308518"
 DEFAULT_FIELDS = ["id", "author_location", "text", "created_at"]
 
 
+
 def get_filtered_stream_tweets(start_date, end_date, fields=DEFAULT_FIELDS):
-
-    if not (start_date >= date(2021, 3, 23) and end_date <= date.today()):
-        raise ValueError("Invalid start date")
-
-    credentials = service_account.Credentials.from_service_account_file(
-        "./BigQuery-SA.json",
-    )
-
     query = f"""
     select
       {','.join(fields)}
@@ -28,19 +21,24 @@ def get_filtered_stream_tweets(start_date, end_date, fields=DEFAULT_FIELDS):
         EXTRACT(DATE from created_at) <= DATE({end_date.year}, {end_date.month}, {end_date.day})
     """
 
-    df = pdbq.read_gbq(query, project_id=PROJECT_ID, credentials=credentials)
-    return df
+    return data_helper(start_date, date(2021, 3, 23), end_date, date.today(), query, fields)
+
+
+def get_vaccine_tweets(start_date, end_date, fields=DEFAULT_FIELDS):
+    query = f"""
+    select
+      {','.join(fields)}
+    from `Tweets.covidtweets2`
+    where
+        EXTRACT(DATE from created_at) >= DATE({start_date.year}, {start_date.month}, {start_date.day})
+      AND
+        EXTRACT(DATE from created_at) <= DATE({end_date.year}, {end_date.month}, {end_date.day})
+    """
+
+    return date_helper(start_date, date(2020, 10, 2), end_date, date(2021, 3, 28), query, fields)
 
 
 def get_general_covid_tweets(start_date, end_date, fields=DEFAULT_FIELDS):
-
-    if not (start_date <= date(2020, 1, 20) and end_date <= date(2021, 3, 28)):
-        raise ValueError("Invalid start date")
-
-    credentials = service_account.Credentials.from_service_account_file(
-        "./BigQuery-SA.json",
-    )
-
     query = f"""
     select
       {','.join(fields)}
@@ -51,5 +49,16 @@ def get_general_covid_tweets(start_date, end_date, fields=DEFAULT_FIELDS):
         EXTRACT(DATE from created_at) <= DATE({end_date.year}, {end_date.month}, {end_date.day})
     """
 
+    return data_helper(start_date, date(2020, 1, 20), end_date, date(2021, 3, 28), query, fields)
+
+def data_helper(start_date, valid_start_date, end_date, valid_end_date, query, fields):
+    if not (start_date <= valid_start_date and end_date <= valid_end_date):
+        raise ValueError("Invalid start date")
+
+    credentials = service_account.Credentials.from_service_account_file(
+        "./BigQuery-SA.json",
+    )
+
     df = pdbq.read_gbq(query, project_id=PROJECT_ID, credentials=credentials)
     return df
+    
